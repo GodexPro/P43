80ae39123d97baac4e0b666a395f62d665c0fe64
GodexPro <74808550+GodexPro@users.noreply.github.com>
2021-05-23 18:28:27 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    text("C U T N I F O N",700,150);
+    text("A T E D A S B A",100,310);
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    text(" Hint : perfoms a particular task !!",696,170);
+    text("Hint : Stores all information !!",100,330);
+    text()
+
+    
+
+}
